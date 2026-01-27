50309b5b70a3122c71dea970e038cb76508ff3f4
Marco Romano <marcojulioromano@gmail.com>
2026-01-27 22:30:55 +0100

    Codecoverage for unit tests (#7)
    * Show unit tests coverage in VSCode
    
    * Create lint and format fix npm tasks
    
    * Update GHA config
    
    * Remove artifacts dir in clean task
    
    * Better exclude for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,5 +5,11 @@ export default defineConfig({
     include: ["tests/unit/**/*.test.ts"],
     exclude: ["tests/e2e.spec.mjs"],
     reporters: ["default", "github-actions"],
+    coverage: {
+      provider: "v8",
+      reportsDirectory: "artifacts/coverage",
+      reporter: ["text", "html", "lcov"],
+      exclude: ["tests/", "dist/", "public/", "*.config.ts"],
+    },
   },
 });
